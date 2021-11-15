adceb33a098a97453005f761ff5d88e38f27bb8b
thamers <simon.thamer@edu.htl-villach.at>
2021-11-15 15:46:26 +0100

    updated to system_threads.

--- src/modulehandler.py
+++ src/modulehandler.py
@@ -5,22 +5,29 @@ from src.modules.module import Module
 from src.device import Device
 from src.utilities import Utilities
 import time
+import os
 
 
 class ModuleHander():
     def __init__(self):
         self._imported_modules = []
         self._workers = {}
+        self._system_threads = {}
+        self._known_system_threads = ["sendData", "checkDevices"]
         self._running = True
         self._api = API()
         self._utilities = Utilities()
         self.mainloop()
 
     def mainloop(self):
-        while self._running:
-            self.check_devices()
-            self.get_data_from_devices()
-            time.sleep(5)
+        try:
+            self.start_system_thread()
+            while self._running:
+                self.check_system_threads()
+                time.sleep(5)
+        except KeyboardInterrupt:
+            os._exit(1)
+        #self.stop_system_thread()
 
     def import_module(self, filename, packagename):
         if packagename not in self._imported_modules:
@@ -29,55 +36,75 @@ class ModuleHander():
             print(f"Successfully imported module {packagename}")
 
     def get_data_from_devices(self):
-        output = {}
-        devices = []
-        for deviceid in self._workers:
-            c_data = self._workers[deviceid].data
-            if c_data:
-                devices.append({"id": deviceid,
-                                "type": self._workers[deviceid].device.type,
-                                "data": c_data})
-        if devices != []:
-            output["devices"] = devices
-            self._api.send_data(output)
-            print(output)
+        while True:
+            output = {}
+            devices = []
+            for deviceid in self._workers:
+                c_data = self._workers[deviceid].data
+                if c_data:
+                    devices.append({"id": deviceid,
+                                    "type": self._workers[deviceid].device.type,
+                                    "data": c_data})
+            if devices != []:
+                output["devices"] = devices
+                self._api.send_data(output)
+                print(output)
+            time.sleep(5)
 
     def check_devices(self):
-        running_devices = self.get_running_devices()
-        devices_to_start = self._utilities.compare_dict(running_devices, self._workers)
-        devices_to_stop = self._utilities.compare_dict(self._workers, running_devices)
-
-        # starts new Devices
-        for c_id in devices_to_start:
-            device_type = running_devices[c_id]["type"]
-            ip = running_devices[c_id]["ip"]
-            name = running_devices[c_id]["name"]
-            timeout = running_devices[c_id]["timeout"]
-            self.import_module(filename=device_type.lower(), packagename=device_type)
-            self.start_device(Device(id=c_id, name=name, type=device_type, ip=ip, timeout=timeout))
-            print(running_devices[c_id])
-
-        # stop devices
-        for c_id in devices_to_stop:
-            self.stop_device(self._workers[c_id].device)
-
-        # update timeout and check for dead devices/threads and restart them
-        for c_id in running_devices:
-            # check if device is still running. if not, restart it.
-            if not self._workers[c_id].is_alive() or self._workers[c_id].is_stopped():
-                c_device = self._workers[c_id].device
-                self.start_device(c_device)
-            # update timeout
-            self._workers[c_id].device.timeout = running_devices[c_id]["timeout"]
+        while True:
+            running_devices = self.get_running_devices()
+            devices_to_start = self._utilities.compare_dict(running_devices, self._workers)
+            devices_to_stop = self._utilities.compare_dict(self._workers, running_devices)
+    
+            # starts new Devices
+            for c_id in devices_to_start:
+                device_type = running_devices[c_id]["type"]
+                ip = running_devices[c_id]["ip"]
+                name = running_devices[c_id]["name"]
+                timeout = running_devices[c_id]["timeout"]
+                self.import_module(filename=device_type.lower(), packagename=device_type)
+                self.start_device(Device(id=c_id, name=name, type=device_type, ip=ip, timeout=timeout))
+                print(running_devices[c_id])
+    
+            # stop devices
+            for c_id in devices_to_stop:
+                self.stop_device(self._workers[c_id].device)
+    
+            # update timeout and check for dead devices/threads and restart them
+            for c_id in running_devices:
+                # check if device is still running. if not, restart it.
+                if not self._workers[c_id].is_alive() or self._workers[c_id].is_stopped():
+                    c_device = self._workers[c_id].device
+                    self.start_device(c_device)
+                # update timeout
+                self._workers[c_id].device.timeout = running_devices[c_id]["timeout"]
+            time.sleep(5)
 
     def start_device(self, device: Device):
         code = f"global cmodule;cmodule = {device.type}(deviceid={device.id},devicetype='{device.type}',devicename='{device.name}',deviceip='{device.ip}', devicetimeout={device.timeout})"
         exec(code, globals())
         self._workers[device.id] = cmodule
-        self._workers[device.id].setDaemon(True)
+        #self._workers[device.id].setDaemon(True)
         self._workers[device.id].start()
         print(f"Started device {device.name}")
         return True
+
+    def start_system_thread(self, key=None):
+        if key == "sendData" or key is None:
+            self._system_threads["sendData"] = threading.Thread(target=self.get_data_from_devices, name="sendData", args=())
+            self._system_threads["sendData"].start()
+        if key == "checkDevices" or key is None:
+            self._system_threads["checkDevices"] = threading.Thread(target=self.check_devices, name="checkDevices", args=())
+            self._system_threads["checkDevices"].start()
+
+    def stop_system_thread(self, key=None):
+        if key is None:
+            for c_key in self._known_system_threads:
+                self._system_threads[c_key].terminate()
+
+    def check_system_threads(self):
+        return None
 
     def stop_device(self, device: Device):
         self._workers[str(device.id)].stop()
@@ -92,4 +119,5 @@ class ModuleHander():
             device_id = c_device["id"]
             del c_device["id"]
             output[device_id] = c_device
+        print(output)
         return output
