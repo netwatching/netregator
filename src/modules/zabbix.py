7931269cc4743ecd831e4871ef41c7a15076cafc
pertlt <tobias.pertl@edu.htl-villach.at>
2021-12-05 19:45:25 +0100

    changed problem handling (returning more problems per device)

--- src/modules/zabbix.py
+++ src/modules/zabbix.py
@@ -4,23 +4,21 @@ from pyzabbix.api import ZabbixAPI
 from decouple import config
 from src.module_data import ModuleData, OutputType
 
-class ZabbixDevice:
-    def __init__(self, hostid: str = None, hostname: str = None, name: str = None, problem: str = None, timestamp: float = None, severity: str = None, tag: list = None):
-        self.hostid = hostid
-        self.hostname = hostname
-        self.name = name
+
+class ZabbixProblems:
+    def __init__(self, problem: str = None, timestamp: float = None, severity: str = None):
         self.problem = problem
         self.timestamp = timestamp
         self.severity = severity
-        self.tag = tag
 
-    @property
-    def hostid(self):
-        return self.__hostid
+    def serialize(self):
+        return {"timestamp": self.timestamp, "severity": self.severity, "data": self.problem}
 
-    @hostid.setter
-    def hostid(self, host_id):
-        self.__hostid = host_id
+
+class ZabbixDevice:
+    def __init__(self, hostname: str = None):
+        self.hostname = hostname
+        self.problems = []
 
     @property
     def hostname(self):
@@ -30,49 +28,11 @@ class ZabbixDevice:
     def hostname(self, host_name):
         self.__hostname = host_name
 
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        self.__name = name
-
-    @property
-    def tag(self):
-        return self.__tag
-
-    @tag.setter
-    def tag(self, host_tag):
-        self.__tag = host_tag
-
-    @property
-    def problem(self):
-        return self.__problem
-
-    @problem.setter
-    def problem(self, problem):
-        self.__problem = problem
-
-    @property
-    def severity(self):
-        return self.__severity
-
-    @severity.setter
-    def severity(self, severity):
-        self.__severity = severity
-
-    @property
-    def timestamp(self):
-        return self.__timestamp
-
-    @timestamp.setter
-    def timestamp(self, timestamp):
-        self.__timestamp = timestamp
-
     def serialize(self):
-        output = {"timestamp": self.timestamp, "severity": self.severity, "problem": self.problem}
-        return output
+        out = []
+        for c_problem in self.problems:
+            out.append(c_problem.serialize())
+        return {self.hostname: out}
 
 
 class Problems(Module):
@@ -88,35 +48,24 @@ class Problems(Module):
 
     def get_hosts(self):
         hosts = self.__connection.host.get(output=["hostid", "host", "name"])
+        print(hosts)
         return hosts
 
     def get_infos(self, hosts):
         zabbix_devices = {}
         for host in hosts:
-            z_device = ZabbixDevice()
+            z_device = ZabbixDevice(hostname=host["host"])
+            # print(self.__connection.problem.get(hostids="10400"))
             problem_obj = self.__connection.problem.get(hostids=host['hostid'], selectHosts='extend')
             if problem_obj:
                 for obj in problem_obj:
-                    z_device.hostid = host["hostid"]
-                    z_device.hostname = host["host"]
-                    z_device.name = host["name"]
-                    z_device.timestamp = obj["clock"]
-                    z_device.severity = obj["severity"]
-                    z_device.problem = obj["name"]
+                    z_problem = ZabbixProblems(problem=obj["name"], severity=obj["severity"], timestamp=obj["clock"])
+                    z_device.problems.append(z_problem)
 
-                tag_obj = self.__connection.host.get(hostids=host['hostid'], selectTags="extend")
-                for obj in tag_obj:
-                    tags = obj['tags']
-                    z_tags = []
-                    for t in tags:
-                        z_tags.append(t['tag'])
-                    z_device.tag = z_tags
-                    #print(z_device.serialize())
                     if not host["host"] in zabbix_devices:
                         zabbix_devices[host["host"]] = []
-                    zabbix_devices[host["host"]].append(z_device.serialize())
+                    zabbix_devices.update(z_device.serialize())
         return zabbix_devices
-
 
     def worker(self):
         hosts = self.get_hosts()
