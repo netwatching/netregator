e7f61f1789637f69d676df114d2ac5acfb116b2c
Simon Thamer <thamers@edu.htl-villach.at>
2021-12-06 17:05:52 +0100

    minor changes

--- src/modulehandler.py
+++ src/modulehandler.py
@@ -41,7 +41,7 @@ class ModuleHander():
             external_events = {}
             for deviceid in self._workers:
                 c_data, c_external_events = self._workers[deviceid].data
-                if c_data != {'static_data': [], 'live_data': [], 'events': {}}:
+                if c_data != None:
                     current_metadata = {"id": deviceid,
                                     "name": self._workers[deviceid].name}
                     current_metadata.update(c_data)
