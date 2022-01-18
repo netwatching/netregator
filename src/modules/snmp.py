e64a195639fdb512aca3b9260ad152224cbb4f24
Thomas Reiter <reitert1@edu.htl-villach.at>
2022-01-18 11:14:46 +0100

    debug: changed to correct community string (test)

--- src/modules/snmp.py
+++ src/modules/snmp.py
@@ -7,23 +7,34 @@ import src.modules.helpers.snmp as snmp
 class SNMP(Module):
     def __init__(self, ip: str = None, timeout: int = None, *args, **kwargs):
         super().__init__(ip, timeout, *args, **kwargs)
+        self.ip = ip
         self.settings = {
             "community_string": "HTL-Villachhh",
-            "c_str_2": "snmp_rw"
+            "c_str_2": "HTL-Villachhh",  # snmp_rw
+            "snmp_port": 161
         }
 
-        self.__ds = snmp.DataSources(snmp.SNMP(self.settings["community_string"], ip, 161))
+        self.__ds = snmp.DataSources(snmp.SNMP(self.settings["community_string"], ip, self.settings["snmp_port"]))
 
     def worker(self):
         # return ModuleData(static_data={}, live_data={}, events={})
         data = {}
 
-        data.update(self.__ds.get_system_data())
-        data.update(self.__ds.get_services())
-        data.update(self.__ds.get_interfaces())
-        # data.update(self.__ds.get_ip_data())
-        data.update(self.__ds.get_ip_addresses())
-        # TODO: add other DataSource functions above
+        try:
+            data.update(self.__ds.get_system_data())
+            data.update(self.__ds.get_services())
+            data.update(self.__ds.get_interfaces())
+            # data.update(self.__ds.get_ip_data())
+            data.update(self.__ds.get_ip_addresses())
+            # TODO: add other DataSource functions above
+        except Exception:
+            self.__ds = snmp.DataSources(snmp.SNMP(self.settings["c_str_2"], self.ip, self.settings["snmp_port"]))
+            data.update(self.__ds.get_system_data())
+            data.update(self.__ds.get_services())
+            data.update(self.__ds.get_interfaces())
+            # data.update(self.__ds.get_ip_data())
+            data.update(self.__ds.get_ip_addresses())
+            # TODO: add other DataSource functions above
 
         # print(data)  # TODO: logging class
         return ModuleData(static_data=data, live_data={}, events={})
