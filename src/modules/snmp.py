dbb057ad8b28fe6a91dd8030f57d0188be7970a3
Thomas Reiter <reitert1@edu.htl-villach.at>
2022-01-30 19:54:36 +0100

    implemented exception handling with warn messasge

--- src/modules/snmp.py
+++ src/modules/snmp.py
@@ -15,7 +15,7 @@ class SNMP(Module):
             "c_str_2": "snmp_rw",  # TODO: replace by proper setting
             "snmp_port": 161
         }
-
+        self._logger = Utilities.setup_logger()
         self.__ds = snmp.DataSources(snmp.SNMP(self.settings["community_string"], ip, self.settings["snmp_port"]))
 
     @staticmethod
@@ -29,20 +29,23 @@ class SNMP(Module):
         data = {}
 
         try:
-            data.update(self.__ds.get_system_data())
-            data.update(self.__ds.get_services())
-            data.update(self.__ds.get_interfaces())
-            # data.update(self.__ds.get_ip_data())
-            data.update(self.__ds.get_ip_addresses())
-            # TODO: add other DataSource functions above
+            try:
+                data.update(self.__ds.get_system_data())
+                data.update(self.__ds.get_services())
+                data.update(self.__ds.get_interfaces())
+                # data.update(self.__ds.get_ip_data())
+                data.update(self.__ds.get_ip_addresses())
+                # TODO: add other DataSource functions above
+            except Exception:
+                self.__ds = snmp.DataSources(snmp.SNMP(self.settings["c_str_2"], self.ip, self.settings["snmp_port"]))
+                data.update(self.__ds.get_system_data())
+                data.update(self.__ds.get_services())
+                data.update(self.__ds.get_interfaces())
+                # data.update(self.__ds.get_ip_data())
+                data.update(self.__ds.get_ip_addresses())
+                # TODO: add other DataSource functions above
         except Exception:
-            self.__ds = snmp.DataSources(snmp.SNMP(self.settings["c_str_2"], self.ip, self.settings["snmp_port"]))
-            data.update(self.__ds.get_system_data())
-            data.update(self.__ds.get_services())
-            data.update(self.__ds.get_interfaces())
-            # data.update(self.__ds.get_ip_data())
-            data.update(self.__ds.get_ip_addresses())
-            # TODO: add other DataSource functions above
+            self._logger.warning(f"SNMP get failed on device with IP: {self.ip}")
 
         # self._logger.spam(data)
         return ModuleData(static_data=data, live_data={}, events={})
